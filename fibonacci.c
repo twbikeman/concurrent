e697a7c63b4c0767713ede0398d2173919e647fa
Bryan <bikeman@mail2000.com.tw>
2020-03-26 11:32:15 +0800

    finish fibonacci

---
+++ fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fib(int x) {
+  if (x == 1 || x == 2)
+    return 1;
+  else
+    return fib(x-1) + fib(x-2);
+}
+
+int main() {
+
+
+  int y = fib(10);
+  printf("%d\n", y);
+  return 0;
+}
