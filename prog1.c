fb03989c86f051ab28e1bf251968884e7225770c
Bryan <twbikeman@gmail.com>
2020-03-28 17:05:54 +0800

    almost finished

--- prog1.c
+++ prog1.c
@@ -94,6 +94,47 @@ void  ellipseArea(int a, int b, int s) {
 }
 
 
+void pinball(int x, int y) {
+
+  char buf[500] = {'\0'};
+  char *bufp = buf;
+  char *head = buf;
+  int *bin = (int *)calloc(x, sizeof(int));
+;  int num;
+  int i, j;
+  srand(time(NULL));
+
+  for(i = 0; i < y; i++) {
+    num = 0;
+    for (j = 0; j < x -1; j++)
+      if ((double)rand()/RAND_MAX > 0.5) num++;
+    bin[num] += 1;
+    /* printf("%d\n", num);     */
+  }
+
+
+  int largest = 0;
+  for(i = 0; i < x; i++)
+  if (largest < bin[i]) largest = bin[i];
+
+  bufp += sprintf(bufp, "Simple Pinball Process Started\n");
+  bufp += sprintf(bufp, "Number of Bin %d\n", x);
+  bufp += sprintf(bufp, "Number of BAll Droppings %d\n", y);
+
+  
+  for(i=0; i < x; i++) {
+     bufp += sprintf(bufp, "%3d-(%7d)-(%5.2f%%)|",i + 1, bin[i], (double)bin[i] / y * 100);
+     for(j = 0; j < (double)bin[i] / largest * 50; j++) {
+     bufp += sprintf(bufp, "*");
+     }
+     bufp += sprintf(bufp, "\n");
+  }
+
+  bufp += sprintf(bufp, "Simple Pinball Process Exits\n");
+  *bufp = '\0';
+  write(1, head , strlen(head));
+}
+
 
 
 
@@ -103,54 +144,74 @@ void  ellipseArea(int a, int b, int s) {
 
 int main(int argc, char *argv[]) {
 
-
+  if (argc != 8) printf("usage errors!/n");
+  
   int n = atoi(argv[1]);
   int r = atoi(argv[2]);
   int a = atoi(argv[3]);  
   int b = atoi(argv[4]);
   int s = atoi(argv[5]);
-  
+  int x = atoi(argv[6]);
+  int y = atoi(argv[7]);
 
   int status;
   pid_t pid;
   printf("Main Proccess Started\n");
-  printf("Fibonacci Input            =  %d\n", n);
+  printf("Fibonacci Input            = %d\n", n);
   printf("Buffon's Needle Iteration  = %d\n", r);
-  printf("\n");
+  printf("Total random Number Pairs  = %d\n", s);
+  printf("Semi-Major Axis Length     = %d\n", a);
+  printf("Semi-Minor Axis Length     = %d\n", b);
+  printf("Number of Bins             = %d\n", x);
+  printf("number of Ball Droppings   = %d\n", y);
 
 
   pid = fork();
   if(pid == 0) {                // child 0
-    sleep(1);
     char buf[80];
     sprintf(buf, "Fibonacci Process Created\n");
     write(1, buf, strlen(buf));
+    sleep(1);
     fib_print(n);
   }
   else {
 
     pid = fork();
     if (pid == 0) {               //child 1
-      sleep(2);
       char buf[80];
       sprintf(buf, "Buffon's Needle Proccess Created\n");
       write(1, buf, strlen(buf));
+
+      sleep(2);
       buffon(r); 
     }
     else {
-      pid = fork();            //chide 2
+      pid = fork();            //chid 2
       if (pid == 0) {
-	sleep(3);
         char buf[80];
 	sprintf(buf, "Ellipse Area Proccess Created\n");
 	write(1, buf, strlen(buf));
+	sleep(3);
 	ellipseArea(a, b, s); 
       }
       else {
-	wait(&status);
-	wait(&status);
-	wait(&status);
-	printf("parent\n");
+	pid = fork();
+	if (pid == 0 ) {  //child 3
+	  char buf[80];
+	  sprintf(buf, "Pinball Process Created\n");
+	  write(1, buf, strlen(buf));
+	  sleep(4);
+	  pinball(x, y);
+	  
+	}
+	else {
+	  printf("Main Process Waits\n");
+	  wait(&status);
+	  wait(&status);
+	  wait(&status);
+	  wait(&status);
+	  printf("Main Process Exits\n");
+	}
       }
 
     }
