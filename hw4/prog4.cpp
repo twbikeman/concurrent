3235c4de619c1900467e0150da864bbdc8aa689a
Bryan Tsai <twbikeman@gmail.com>
2020-05-20 05:54:04 +0800

    fail

--- hw4/prog4.cpp
+++ hw4/prog4.cpp
@@ -24,6 +24,8 @@ int pots_status_g[100];
 
 int child_to_call = 0;
 
+int offer_meals_g;
+
 
 void delay() {
   sleep(1);
@@ -37,9 +39,14 @@ int ready_to_eat(int id){
      int exit_flag = 0;
 
    sem_wait(&sem_children);
+   sprintf(message, "%*sBaby eagle %d is ready to eat.\n", id, "", id);
+   write(1, message, strlen(message));
+   message[0] = '\0';
+
 
    while (1) {
-   
+
+     
      pthread_mutex_lock(&mutex_thread);
 
      int two_exist = 0, all_zero = 0 ;
@@ -64,15 +71,17 @@ int ready_to_eat(int id){
      else if (all_zero == 1) {
        if (child_to_call == 0) {
        sprintf(message, "%*sBaby %d see all feeding pots are empty and wakes up the mother.\n", id, "", id);
+       sem_post(&sem_mother);
        child_to_call = id;
+       
        }
        else {
-	 sprintf(message, "");
+	 message[0] = '\0';
        }
        exit_flag = 0;
      }
      else {
-       sprintf(message, "");
+       message[0] = '\0';
        exit_flag = 0;
      }
      
@@ -81,7 +90,7 @@ int ready_to_eat(int id){
      delay();
      if (exit_flag == 1) break;
    }
-   sem_post(&sem_children);
+
    return pot;
    
  };
@@ -89,11 +98,10 @@ int ready_to_eat(int id){
 
 void finish_eating(int id, int pot) {
   char message[80];
-  pthread_mutex_lock(&mutex_thread);
   pots_status_g[pot] = 0;
   sprintf(message, "%*sBaby eagle %d finishes eating.\n", id, "", id);
   write(1, message, strlen(message));
-  pthread_mutex_unlock(&mutex_thread);
+  sem_post(&sem_children);
 };
 
 void * children_actions(void *children_id) {
@@ -113,6 +121,45 @@ void * children_actions(void *children_id) {
   }
 };
 
+void goto_sleep(){
+    char message[80];
+    sprintf(message, "Mother eagle takes a nap.\n");
+    write(1, message, strlen(message));
+
+};
+
+void food_ready() {
+  char message[80];
+  sprintf(message, "Mother eagle is awoked by baby eagle %d and starts preparing food.\n", child_to_call );
+  write(1, message, strlen(message));
+  int i;
+  pthread_mutex_lock(&mutex_thread);
+  for (i = 0; i < m_g; i++) {
+    pots_status_g[i] = 2; // 0: empty; 1:occupied, 2:available;
+  }
+
+  offer_meals_g--;
+
+  message[0] = '\0';
+  sprintf(message, "Mother eagle says \"Feeding (%d)\"\n", t_g - offer_meals_g);
+  write(1, message, strlen(message));
+  pthread_mutex_unlock(&mutex_thread);
+    
+
+};
+
+void * mother_actions(void *) {
+    char message[80];
+    sprintf(message, "Mother eagle started.\n");
+    write(1, message, strlen(message));
+    while(1) {
+      goto_sleep();
+      sem_wait(&sem_mother);
+      delay();
+      food_ready();
+    }
+
+}
 
 
 
@@ -120,7 +167,7 @@ int main(int argc, char **argv) {
   int m, n, t; // m: feeding pots; n: baby eagles; t: refill times
   m_g =m = atoi(argv[1]);
   n_g = n = atoi(argv[2]);
-  t_g = t = atoi(argv[3]);
+  offer_meals_g = t_g = t = atoi(argv[3]);
   //  cout << " m: " << m << " n: " << n << " t: " << t << endl;
   srand(time(NULL));
   
@@ -147,13 +194,18 @@ int main(int argc, char **argv) {
     children_ids[i] = i + 1;
     pthread_create(&thread_childrean[i], NULL, children_actions, (void *)&children_ids[i]);
   }
+
+  pthread_create(&thread_mother, NULL, mother_actions, NULL);
   
   sem_init(&sem_children, 0, m);
-  sem_init(&sem_mother, 0, 1);
+  sem_init(&sem_mother, 0, 0);
 
 
   for (i = 0; i < n; i++)
     pthread_join(thread_childrean[i], NULL);
+
+  pthread_join(thread_mother, NULL);
+  
 
   
   return 0;
